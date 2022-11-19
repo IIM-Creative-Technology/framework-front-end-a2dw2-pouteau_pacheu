0429ca9305298c031a91843732ffb7a89d7b87bc
Pouteau arthur <arthur.pouteau92@gmail.com>
2022-11-19 14:12:50 +0100

    almost finishedheader

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import Nav from "./components/Nav.vue";
 
 <template>
 
-  <div class="bg-gradient-to-r from-cyan-500 to-blue-500 lg:px-20">
-    <div class="py-7 px-2 flex flex-row justify-between items-center">
+  <div class="bg-gradient-to-r from-cyan-500 to-blue-500 lg:px-20 px-4">
+    <div class="py-10 flex flex-row justify-between items-center">
       <img src="/image2.png" />
       
       <div class="hidden lg:flex flex-row justify-between items-center w-full">
@@ -32,6 +32,20 @@ import Nav from "./components/Nav.vue";
         <img class="py-0.5" src="/Vector6.png">
       </div>
     </div>
+    <h1 class="text-center text-txt_white font-bold text-2xl lg:text-4xl mb-3 lg:mb-6">Découvrez nos formations sur étagère !</h1>
+    <p class="text-center text-txt_white max-w-4xl m-auto">Notre catalogue de formations soft skills est le meilleur allié de vos managers et collaborateurs. Management, vente, ressources humaines, efficacité professionnelle... Composez votre parcours de formation et mixez les modalités pédagogiques pour développer leurs compétences.</p>
+
+    <div class="flex flex-col lg:flex-row gap-8 rounded-3xl bg-txt_white py-8 px-4 mt-20 translate-y-40>
+      <div class="flex items-center justify-between py-2 bg-bg_ligth_grey rounded-full py-4" px-2>
+        <p>Quelle formation recherchez vous ?</p>
+        <img src="/IMG_7142.png">
+      </div>
+      <div class="flex items-center justify-between py-2 bg-bg_ligth_grey rounded-full py-4  px-2">
+        <p>Filtrez</p>
+        <img src="/Vector.png">
+      </div>
+    </div>
+
   </div>
 
 
